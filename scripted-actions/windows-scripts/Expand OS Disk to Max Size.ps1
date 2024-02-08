07641f5d25603ed4731f398f8f721654cf537485
hwwilliams <hunter@westwilliams.com>
2024-02-07 23:35:11 -0500

    Add expand OS disk script

---
+++ scripted-actions/windows-scripts/Expand OS Disk to Max Size.ps1
@@ -0,0 +1,12 @@
+#description: Expands OS disk to maximum allowed volume size, useful when increasing the size beyond 128GB.
+#execution mode: IndividualWithRestart
+#tags: ITaaS
+<#
+Expands OS disk to maximum allowed volume size, useful when increasing the size beyond 128GB.
+#>
+
+$DiskCPartition = Get-Partition -DriveLetter "C"
+$DiskCPartitionSupportedSize = Get-PartitionSupportedSize -DriveLetter "C"
+if ($DiskCPartition.Size -lt $DiskCPartitionSupportedSize.SizeMax) {
+    Resize-Partition -DriveLetter "C" -Size $DiskCPartitionSupportedSize.SizeMax
+}
