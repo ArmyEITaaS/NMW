b8b56e442cdafb5d80502fd1f282708c1e227f80
hwwilliams <hunter@westwilliams.com>
2024-03-18 14:12:30 -0400

    Update

--- scripted-actions/windows-scripts/Test Error.ps1
+++ scripted-actions/windows-scripts/Test Error.ps1
@@ -1,3 +1,10 @@
+#description: Test output and error
+#execution mode: Individual
+#tags: ITaaS
+<#
+Test output and error
+#>
+
 Write-Host "Writing to host"
 Write-Output "Writing to output"
 Write-Error "writing to error"
