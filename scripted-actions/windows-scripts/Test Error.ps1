59f06a469cc7f6b26e74f402c2023d63edd862c1
hwwilliams <hunter@westwilliams.com>
2024-03-18 14:33:38 -0400

    Update

--- scripted-actions/windows-scripts/Test Error.ps1
+++ scripted-actions/windows-scripts/Test Error.ps1
@@ -5,14 +5,9 @@
 Test output and error
 #>
 
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+
 Write-Host "Writing to host"
 Write-Output "Writing to output"
 Write-Error "writing to error"
 throw "throw error"
-
-$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
-
-Write-Host "Writing to host 2"
-Write-Output "Writing to output 2"
-Write-Error "writing to error 2"
-throw "throw error 2"
