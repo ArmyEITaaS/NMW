1421e91affbe98ebc028aa1531cec369cf890a49
hwwilliams <hunter@westwilliams.com>
2024-03-20 18:57:47 -0400

    Update

--- scripted-actions/windows-scripts/Set-HostFile.ps1
+++ scripted-actions/windows-scripts/Set-HostFile.ps1
@@ -40,19 +40,23 @@ $HostEntries = @{
     }
 }
 
-$AzureVMName = "AVDVAINT01AIMG"
+Write-Output "Finding ring slice and region for VM '$AzureVMName'"
 [Regex] $AzureVMNameRegex = "AVD(AZ|TX|VA)([A-Z]{3})(0[1-9]|[1-9][0-9])([A-B]{1})IMG"
 $AzureVMNameRegexMatchedValues = $AzureVMNameRegex.match($AzureVMName).Groups.Value
 $AzureVMRingSlice = $AzureVMNameRegexMatchedValues[2..4] -join ""
 $AzureVMLocation = $AzureVMNameRegexMatchedValues[1]
 
+Write-Output "Found ring slice '$AzureVMRingSlice' and region '$AzureVMLocation' for VM '$AzureVMName'"
 $AzureVMHostEntries = $HostEntries.$AzureVMRingSlice.$AzureVMLocation
+Write-Output "Found $($AzureVMHostEntries.Count) host entries to add to VM '$AzureVMName'"
 
 $UTCDateTime = Get-Date (Get-Date).ToUniversalTime() -UFormat '+%Y%m%dT%H%MZ'
 $HostsFilePath = "$env:windir\System32\drivers\etc\hosts"
 $BackupHostsFilePath = "${HostsFilePath}.${UTCDateTime}.bak"
 Move-Item -Path $HostsFilePath -Destination $BackupHostsFilePath -Force
 
-foreach ($HostEntry in $AzureVMHostEntries) {
-    "$($HostEntry.IPAddress.Trim()) $($HostEntry.FQDN.Trim())" | Tee-Object -FilePath $HostsFilePath -Append
+foreach ($AzureVMHostEntry in $AzureVMHostEntries) {
+    $HostEntry = "$($AzureVMHostEntry.IPAddress.Trim()) $($AzureVMHostEntry.FQDN.Trim())"
+    Write-Output "Adding entry '$HostEntry' to host file for VM '$AzureVMName'"
+    $HostEntry | Tee-Object -FilePath $HostsFilePath -Append
 }
