55916aebe0326c93d8609968bfbfb10fa27191e1
hwwilliams <hunter@westwilliams.com>
2024-03-20 14:51:03 -0400

    Add

--- scripted-actions/windows-scripts/Set-HostFile.ps1
+++ scripted-actions/windows-scripts/Set-HostFile.ps1
@@ -5,10 +5,9 @@ Notes:
 This script will set the contents of the hosts file on the VM.
 #>
 
-$HostEntries = @(
-    @{
-        ResourceGroupName = "P-ARMY-AVD-VA-01-INT01A-1"
-        Entries           = @(
+$HostEntries = @{
+    "INT01A" = @{
+        "VA" = @(
             @{
                 IPAddress = "172.23.255.228"
                 FQDN      = "parmyavdvaint01fx1.blob.core.usgovcloudapi.net"
@@ -23,9 +22,8 @@ $HostEntries = @(
             }
         )
     }
-    @{
-        ResourceGroupName = "P-ARMY-AVD-VA-01-INT01B-1"
-        Entries           = @(
+    "INT01B" = @{
+        "VA" = @(
             @{
                 IPAddress = "172.23.255.244"
                 FQDN      = "parmyavdvaint01fx1.blob.core.usgovcloudapi.net"
@@ -40,15 +38,21 @@ $HostEntries = @(
             }
         )
     }
-)
+}
 
-$ResourceGroupHostEntries = $HostEntries.Where({ $PSItem.ResourceGroupName -eq "P-ARMY-AVD-VA-01-INT01B-1" }).Entries
+$AzureVMName = "AVDVAINT01AIMG"
+[Regex] $AzureVMNameRegex = "AVD(AZ|TX|VA)([A-Z]{3})(0[1-9]|[1-9][0-9])([A-B]{1})IMG"
+$AzureVMNameRegexMatchedValues = $AzureVMNameRegex.match($AzureVMName).Groups.Value
+$AzureVMRingSlice = $AzureVMNameRegexMatchedValues[2..4] -join ""
+$AzureVMLocation = $AzureVMNameRegexMatchedValues[1]
+
+$AzureVMHostEntries = $HostEntries.$AzureVMRingSlice.$AzureVMLocation
 
 $UTCDateTime = Get-Date (Get-Date).ToUniversalTime() -UFormat '+%Y%m%dT%H%MZ'
 $HostsFilePath = "$env:windir\System32\drivers\etc\hosts"
 $BackupHostsFilePath = "${HostsFilePath}.${UTCDateTime}.bak"
 Move-Item -Path $HostsFilePath -Destination $BackupHostsFilePath -Force
 
-foreach ($HostEntry in $ResourceGroupHostEntries) {
+foreach ($HostEntry in $AzureVMHostEntries) {
     "$($HostEntry.IPAddress.Trim()) $($HostEntry.FQDN.Trim())" | Tee-Object -FilePath $HostsFilePath -Append
 }
