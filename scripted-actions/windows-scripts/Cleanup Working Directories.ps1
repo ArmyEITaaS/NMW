d6996570e4e73c7d7f907bf7998e1a5f04668fa1
hwwilliams <hunter@westwilliams.com>
2024-04-20 02:06:43 -0400

    Add

---
+++ scripted-actions/windows-scripts/Cleanup Working Directories.ps1
@@ -0,0 +1,15 @@
+#description: Cleanup working directories
+#execution mode: Individual
+#tags: ITaaS
+<#
+Cleanup working directories.
+#>
+
+Write-Output "Cleaning up working directories"
+Remove-Item -Path "C:\AzureData\*" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\DSC\*" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\LGPO-STIGs" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\logs\*" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\temp\*" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\virtual-desktop-optimization-tool\" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\WindowsDefenderATPOnboardingScript.cmd" -Force -ErrorAction SilentlyContinue
--- scripted-actions/windows-scripts/Set-HostFile.ps1
+++ scripted-actions/windows-scripts/Set-HostFile.ps1
@@ -5,50 +5,20 @@ Notes:
 This script will set the contents of the hosts file on the VM.
 #>
 
-$HostEntries = @{
-    "INT01A" = @{
-        "VA" = @(
-            @{
-                IPAddress = "172.23.255.229"
-                FQDN      = "parmyavdvaint01fslogix1.file.core.usgovcloudapi.net"
-            }
-            @{
-                IPAddress = "52.127.58.160"
-                FQDN      = "power-apis-usdod-001.azure-apihub.us"
-            }
-        )
+$HostEntries = @(
+    @{
+        IPAddress = "52.127.58.160"
+        FQDN      = "power-apis-usdod-001.azure-apihub.us"
     }
-    "INT01B" = @{
-        "VA" = @(
-            @{
-                IPAddress = "172.23.255.245"
-                FQDN      = "parmyavdvaint01fslogix1.file.core.usgovcloudapi.net"
-            }
-            @{
-                IPAddress = "52.127.58.160"
-                FQDN      = "power-apis-usdod-001.azure-apihub.us"
-            }
-        )
-    }
-}
-
-Write-Output "Finding ring slice and region for VM '$AzureVMName'"
-[Regex] $AzureVMNameRegex = "AVD(AZ|TX|VA)([A-Z]{3})(0[1-9]|[1-9][0-9])([A-B]{1})IMG"
-$AzureVMNameRegexMatchedValues = $AzureVMNameRegex.match($AzureVMName).Groups.Value
-$AzureVMRingSlice = $AzureVMNameRegexMatchedValues[2..4] -join ""
-$AzureVMLocation = $AzureVMNameRegexMatchedValues[1]
-
-Write-Output "Found ring slice '$AzureVMRingSlice' and region '$AzureVMLocation' for VM '$AzureVMName'"
-$AzureVMHostEntries = $HostEntries.$AzureVMRingSlice.$AzureVMLocation
-Write-Output "Found $($AzureVMHostEntries.Count) host entries to add to VM '$AzureVMName'"
+)
 
 $UTCDateTime = Get-Date (Get-Date).ToUniversalTime() -UFormat '+%Y%m%dT%H%MZ'
 $HostsFilePath = "$env:windir\System32\drivers\etc\hosts"
 $BackupHostsFilePath = "${HostsFilePath}.${UTCDateTime}.bak"
 Move-Item -Path $HostsFilePath -Destination $BackupHostsFilePath -Force
 
-foreach ($AzureVMHostEntry in $AzureVMHostEntries) {
-    $HostEntry = "$($AzureVMHostEntry.IPAddress.Trim()) $($AzureVMHostEntry.FQDN.Trim())"
-    Write-Output "Adding entry '$HostEntry' to host file for VM '$AzureVMName'"
+foreach ($HostEntry in $HostEntries) {
+    $HostEntry = "$($HostEntry.IPAddress.Trim()) $($HostEntry.FQDN.Trim())"
+    Write-Output "Adding entry '$HostEntry' to host file"
     $HostEntry | Tee-Object -FilePath $HostsFilePath -Append
 }
